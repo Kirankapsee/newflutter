feac7e78b7fd3d4aeac9dd800c87b7c0de4e224b
admin <admin@local>
2026-06-13 19:34:40 +0530

    cjanges done

---
+++ lib/file.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class Sagar extends StatefulWidget {
+  const Sagar({super.key});
+
+  @override
+  State<Sagar> createState() => _SagarState();
+}
+
+class _SagarState extends State<Sagar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/new.dart
+++ lib/new.dart
@@ -8,3 +8,20 @@ class Vipul extends StatelessWidget {
     return const Placeholder();
   }
 }
+
+class Chikate extends StatefulWidget {
+  const Chikate({super.key});
+
+  @override
+  State<Chikate> createState() => _ChikateState();
+}
+
+class _ChikateState extends State<Chikate> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
+
